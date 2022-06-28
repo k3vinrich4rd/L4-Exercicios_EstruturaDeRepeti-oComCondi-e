cbdf7f893645e0115aa78a45827f06d60a439165
k3vinrich4rd <kevinrichardve@gmail.com>
2022-06-28 15:11:58 -0300

    Inserir código resolvido

---
+++ src/l4/Exercicio5.java
@@ -0,0 +1,21 @@
+package l4;
+
+import java.util.Scanner;
+
+public class Exercicio5 {
+    public static void main(String[] args) {
+        int numerosImpares = 0;
+        for (int i = 0; i <= 50 ; i++) {
+            if (i % 2 != 0){
+                numerosImpares = i;
+                System.out.println(numerosImpares);
+            }
+
+        }
+
+            
+        }
+
+
+    }
+
