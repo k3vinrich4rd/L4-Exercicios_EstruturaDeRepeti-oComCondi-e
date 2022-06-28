e02b60a7bd8747e2f0bdaa916d11a77b43afd7f5
k3vinrich4rd <kevinrichardve@gmail.com>
2022-06-28 12:34:39 -0300

    Inserindo outra forma de se realizar uma estrutura de repetição

--- src/l4/Exercicio3Pt2fori.java
+++ src/l4/Exercicio3Pt2fori.java
@@ -1,2 +1,24 @@
-package l4;public class Exercicio4Pt2fori {
+package l4;
+
+import java.util.Scanner;
+// Outra forma de se fazer a tabuada utilizando o (fori):
+
+public class Exercicio3Pt2fori {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Qual número você deseja ver a tabuada: ");
+        int numero = input.nextInt();
+
+        int x = numero;
+        int y = 0;
+
+        for ( x = 1; x <= 10; x++) {
+            y = x * numero;
+            System.out.println(numero + " x " + x + " = " + x);
+
+        }
+    }
 }
