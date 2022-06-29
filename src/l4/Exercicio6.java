239c0e3a6767bae8d523ad401b14ade97b85642f
k3vinrich4rd <kevinrichardve@gmail.com>
2022-06-28 21:55:42 -0300

    Inserindo resolução do código

---
+++ src/l4/Exercicio6.java
@@ -0,0 +1,24 @@
+package l4;
+
+import java.util.Scanner;
+
+public record Exercicio6() {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Digite o primeiro número inteiro: ");
+        int primeiroNumeroInteiro = input.nextInt();
+        System.out.print("Digite o segundo número inteiro: ");
+        int segundoNumeroInteiro = input.nextInt();
+        input.close();
+
+        int intervalo = 0;
+
+        for (int i = primeiroNumeroInteiro +1; i < segundoNumeroInteiro; i++) {
+            intervalo = i;
+            System.out.print("Os números nos intervalos são: ");
+            System.out.println(intervalo);
+        }
+
+
+    }
+}
